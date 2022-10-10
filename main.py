1795014c6c17a341cdaf0e2c8d835145dd8f63fb
KRjabinov <114918656+Klim0n@users.noreply.github.com>
2022-10-10 11:45:18 +0000

    2 uzd

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-
-sk1=int(input("ievadirtskaitli: "))
-if sk1>10:
-  print("skaitlis ir liels")
+from math import *
+a=int(input("ievadirtskaitli a: "))
+b=int(input("ievadirtskaitli b: "))
+c=int(input("ievadirtskaitli c: "))
+D=b**2-4*a*c
+if D<0:
+  print("skaņu nav!")
+elif D==0:
+  x=-b/2*a
+  print("ir viena sakne =",x)
 else:
-  print("skaitlis ir mazs")
+  x1=(-b+sqrt(D))/2*a 
+  x2=(-b+sqrt(D))/2*a
+  print("ir divas saknes, x1 =",x1," x2 =",x2)
